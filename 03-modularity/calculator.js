42a8a87c83c0fdf31aac9308a8f62ccd0c51f9e4
Magesh <tkmagesh77@gmail.com>
2018-03-05 14:39:44 +0530

    modularity

--- 03-modularity/calculator.js
+++ 03-modularity/calculator.js
@@ -21,12 +21,6 @@ var calculator = {
 	divide(x,y){
 		return x / y;
 	}
-}
-
-var x = 100, y = 50;
-console.log(calculator.add(x,y));
-console.log(calculator.subtract(x,y));
-console.log(calculator.multiply(x,y));
-console.log(calculator.divide(x,y));
-
-
+};
+console.log('[@calculator] calculator = ', calculator);
+module.exports = calculator;
