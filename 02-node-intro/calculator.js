13672349c0f9753860b30c163a6c687d35fc3e98
Magesh <tkmagesh77@gmail.com>
2018-03-05 14:01:27 +0530

    calculator

---
+++ 02-node-intro/calculator.js
@@ -0,0 +1,30 @@
+/*
+create a node program called calculator.js
+	create a calculator object with the following methods
+		-add(x,y) => result
+		-subtract(x,y) => result
+		-multiply(x,y) => result
+		-divide(x,y) => result
+
+	call all the methods of the calculator for x = 100 and y = 50 and print the results
+*/
+var calculator = {
+	add : function(x,y){
+		return x + y;
+	},
+	subtract(x,y){
+		return x - y;
+	},
+	multiply(x,y){
+		return x * y;
+	},
+	divide(x,y){
+		return x / y;
+	}
+}
+
+var x = 100, y = 50;
+console.log(calculator.add(x,y));
+console.log(calculator.subtract(x,y));
+console.log(calculator.multiply(x,y));
+console.log(calculator.divide(x,y));
